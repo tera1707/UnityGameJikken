dcea8dc8bdd520dfc9f0da9fc74ae882e4c084bf
tera1707 <Masa1017>
2025-10-19 01:19:20 +0900

    接地しているときだけジャンプできるようにした

--- MyFirstU3D/Assets/Scripts/PlayerController.cs
+++ MyFirstU3D/Assets/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -9,6 +10,9 @@ public class PlayerController : MonoBehaviour
     public float moveAmount = 4.0f;
     public float jumpAmount = 20.0f;
 
+    // 設置チェック
+    private float distance;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,11 +20,16 @@ public class PlayerController : MonoBehaviour
         rb = gameObject.GetComponent<Rigidbody>();
 
         Physics.gravity *= 5;
+
+        distance = 0.5f;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Rayが見たいので見るためだけに呼ぶ
+        GetIsGrounded();
+
         float lx = Gamepad.current.leftStick.ReadValue().x;
         float ly = Gamepad.current.leftStick.ReadValue().y;
         float rx = Gamepad.current.rightStick.ReadValue().x;
@@ -44,8 +53,21 @@ public class PlayerController : MonoBehaviour
 
         if (Gamepad.current.buttonEast.wasPressedThisFrame)
         {
-            // 真上に、相手の質量の1000倍の力を加える
-            rb.AddForce(Vector3.up * rb.mass * jumpAmount, ForceMode.Impulse);
+            if (GetIsGrounded())
+            {
+                // 真上に、相手の質量の1000倍の力を加える
+                rb.AddForce(Vector3.up * rb.mass * jumpAmount, ForceMode.Impulse);
+            }
         }
+    }
+
+    private bool GetIsGrounded()
+    {
+        Vector3 rayPosition = transform.position + new Vector3(0.0f, 0.0f, 0.0f);
+        Ray ray = new Ray(rayPosition, Vector3.down);
+        bool isGround = Physics.Raycast(ray, distance);
+        Debug.DrawRay(rayPosition, Vector3.down * distance * 10, Color.red);
+
+        return isGround;
     }
 }
